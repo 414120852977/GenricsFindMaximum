aae6f7cc2eaf232f7812ee7492ad75e1545c1236
Ashok Mane <ashokmane81800@gmail.com>
2021-04-27 12:37:56 +0530

    [ashok] added a Find Maximum Value Using Genrics

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -3,20 +3,20 @@ package com.bridgelabz;
 public class Maximum {
 
 
-//    public  static <E extends  Comparable<E>> E printGenric(E a, E b, E c) {
-//        E max = a;
-//        if(b.compareTo(max)>0) {
-//            max = b;
-//        }
-//        if(c.compareTo(max)>0) {
-//            max = c;
-//        }
-//        printGenrics(a,b,c,max) ;
-//        return max;
-//    }
-//
-//    private static <E extends  Comparable<E>> void printGenrics(E a, E b, E c, E max) {
-//        System.out.println("maximum value is :"+max);
-//    }
+    public  static <E extends  Comparable<E>> E printGenric(E a, E b, E c) {
+        E max = a;
+        if(b.compareTo(max)>0) {
+            max = b;
+        }
+        if(c.compareTo(max)>0) {
+            max = c;
+        }
+        printGenrics(a,b,c,max) ;
+        return max;
+    }
+
+    private static <E extends  Comparable<E>> void printGenrics(E a, E b, E c, E max) {
+        System.out.println("maximum value is :"+max);
+    }
 
 }
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -7,9 +7,9 @@ public class MaximumTest {
     public static void main(String[] args) {
         System.out.println("welcome to find maximum using genrics");
     }
-//    @Test
-//    public void givenIntegershouldGivesMaximumAtFirstPlace() {
-//        int result = Maximum.printGenric(9,2,8);
-//        Assert.assertEquals(9,result);
-//    }
+    @Test
+    public void givenIntegershouldGivesMaximumAtFirstPlace() {
+        int result = Maximum.printGenric(9,2,8);
+        Assert.assertEquals(9,result);
+    }
 }
