538fa4503dd977924a49332060c4ae4a46d02062
Ashok Mane <ashokmane81800@gmail.com>
2021-04-27 12:29:00 +0530

    [ashok] created a welcome message

---
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaximumTest {
+    public static void main(String[] args) {
+        System.out.println("welcome to find maximum using genrics");
+    }
+//    @Test
+//    public void givenIntegershouldGivesMaximumAtFirstPlace() {
+//        int result = Maximum.printGenric(9,2,8);
+//        Assert.assertEquals(9,result);
+//    }
+}
